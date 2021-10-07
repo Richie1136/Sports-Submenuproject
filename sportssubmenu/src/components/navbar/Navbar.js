aceb991ec18016078643ebe47e34f6bba5db4fdd
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-10-07 11:31:15 -0400

    Working on styling

--- sportssubmenu/src/components/navbar/Navbar.js
+++ sportssubmenu/src/components/navbar/Navbar.js
@@ -9,11 +9,25 @@ const Navbar = () => {
     const tempBt = e.target.getBoundingClientRect()
     const center = (tempBt.left + tempBt.right) / 2
     const bottom = tempBt.bottom - 3
+    console.log(bottom)
+    openSubmenu(section, { center, bottom })
   }
   return (
-    <div>
-      <h2>Nav component</h2>
-    </div>
+    <nav className='nav'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+        </div>
+        <li>
+          <button className='link-btn' onMouseOver={displaySubmenu}>Baseball</button>
+        </li>
+        <li>
+          <button className='link-btn' onMouseOver={displaySubmenu}>Basketball</button>
+        </li>
+        <li>
+          <button className='link-btn' onMouseOver={displaySubmenu}>Football</button>
+        </li>
+      </div>
+    </nav>
   )
 }
 
