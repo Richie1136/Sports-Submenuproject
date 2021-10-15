4c39e2d33626898359a9bbc21a61b7bf70b1ac47
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-10-15 14:52:15 -0400

    Finished colorado, cubs,detroit and working of the dodgers

--- sportssubmenu/src/components/mlbteams/Colorado.js
+++ sportssubmenu/src/components/mlbteams/Colorado.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import logo from '../../images/bieber.png'
-import logo2 from '../../images/ramirez.png'
+import logo from '../../images/story.png'
+import logo2 from '../../images/cron.png'
 import '../styles/Colorado.css'
 
 const Colorado = () => {
@@ -10,23 +10,25 @@ const Colorado = () => {
       <p className='rockies'>The Colorado Rockies are a Major League Baseball team. They are based in Denver, Colorado.
         They are a member of the West Division of Major League Baseball's National League. The Rockies colors
         are Rockies Purple, Silver, and Midnight Black. The Rockies began in 1992.
-        The Rockies finished the regular season with a 74-87 record. Due to the thin air in Colorado, The ball travels much further which buffs hitters numbers. If players hit really well in Colorado people worry, how good that specific player vs how much were those numbers buffed due to where he played.</p>
+        The Rockies finished the regular season with a 74-87 record. Due to the thin air in Colorado, The ball travels much further which buffs hitters numbers. If players hit really well in Colorado people worry, how good that specific player vs how much were those numbers buffed due to where he played.
+        The lack of quality pitching has been a major issue for the organization for years.
+      </p>
       <br />
       <p className='story'>
-        The Guardians have lost alot of talent over the last couple of years but they still have two very good players who stand out on the roster.
-        One of those players being Shane Bieber.
-        Story is a two-time Mlb All-Star; one-time Cy Young Award Winner, 2020 AL ERA leader.
-        Trevor Story was drafted in the 4th round of the 2016 Mlb draft by the Indians.
-        Bieber has a career 3.29 ERA, 633 strikeouts and a 11.3 K/9.
+        Trevor Story has been one of the rockies best players for years now.
+        Trevor Story was a mid season trade candidate this past season due to the fact that he could bring some value back to the rockies organization and is a free agent, but no trade ever came about.
+        Story is a two-time Mlb All-Star; two-time NL Silver Slugger Award winner; 2018 NL Extra Base Hits leader.
+        Trevor Story was drafted in the 1st round of the 2011 Mlb draft by the Rockies.
+        Story has a career .272 BA 158 HR .340 OBP .523 SLG .863 OPS
       </p>
       <img className='img1' src={logo} alt='img1' />
       <br />
       <p className='cron'>
-        The second player who is an elite infielder is Jose Ramirez.
-        Jose Ramirez has played every position on the field except for 1st base, pitcher and catcher.
-        Jose Ramirez is a three-time Mlb All-Star; Three-time winner of the Silver Slugger award, 2017 AL Extra Base Hits leader and 2020 AL Extra Base Hits leader.
-        Jose Ramirez was signed by the Cleveland Indians Braves in November of 2009 as an amateur free agent.
-        Ramirez has a career .278 BA 163 HR .354 OBP .501 SLG .855 OPS
+        The second player who I selected from the Rockies roster is CJ Cron whos full name is Christopher John Cron.
+        CJ Cron is a 1st baseman.
+        CJ Cron is a 2021 August NL Player of the Month winner and 2021 August 8th NL Player of the Week winner.
+        CJ Cron was drafted in the 1st round of the 2011 Mlb draft by the Angels.
+        Cron has a career .261 BA 146 HR .323 OBP .475 SLG .798 OPS
         <img className='img2' src={logo2} alt='img2' />
       </p>
     </div >
--- sportssubmenu/src/components/mlbteams/Detroit.js
+++ sportssubmenu/src/components/mlbteams/Detroit.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import logo from '../../images/candelario.png'
+import logo2 from '../../images/mize.png'
+import '../styles/Detroit.css'
 
 const Detroit = () => {
   return (
-    <div>
-      <h2>Detroit</h2>
-    </div>
+    <div className='det'>
+      <h1>Detroit Tigers</h1>
+      <p className='tigers'>The Detroit Tigers are a Major League Baseball team. They are based in Detroit, Michigan.
+        They are a member of the Central Division of Major League Baseball's American League. The Tigers colors
+        are Navy, and Orange. The Tigers began in 1901.
+        The Detroit Tigers are the oldest American League baseball team to have only one home city and one name in their history. Beginning in 1894, the Tigers played at Boulevard Park as a charter member in the Western League.
+        The Tigers finished the regular season with a 77-85 record. Although the tigers finished 8 games below .500 the future is extremely bright for this organization.</p>
+      <br />
+      <p className='candelario'>
+        Jeimer Candelario is one of the longest tenure tigers and has been a solid player for years, and this best season being his best.
+        Jeimer Candelario is a 1st and 3rd baseman.
+        Jeimer Candelario was signed by the Cubs in November of 2010 as a free agent.
+        Candelario won 2020 September 13th AL Player of the Week winner and 2021 AL Doubles leader.
+        Candelario has a career .246 BA 53 HR .334 OBP .410 SLG .744 OPS.
+      </p>
+      <img className='img1' src={logo} alt='img1' />
+      <br />
+      <p className='mize'>
+        Casey Mize was the best pitcher of the tigers this past season and is projected to be the ace of the future for the tigers. After being selected #1 overall in the 2018 MLB draft and dominating the minor leagues, alot of hype was surrounding him.
+        Casey Mize threw one complete game.
+        Mize pitching numbers this past season 3.71 ERA 118 strikeouts and a 7.1 K/9.
+        <img className='img2' src={logo2} alt='img2' />
+      </p>
+    </div >
   )
 }
 
