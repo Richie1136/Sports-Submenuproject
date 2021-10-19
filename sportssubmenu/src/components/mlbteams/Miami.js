d161f4d5675abae1c402eee5980754d850a7c2ce
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-10-19 14:56:28 -0400

    Giving information about all the divisons and the best players from each division

--- sportssubmenu/src/components/mlbteams/Miami.js
+++ sportssubmenu/src/components/mlbteams/Miami.js
@@ -1,30 +1,32 @@
 import React from 'react'
-import logo from '../../images/degrom.png'
-import logo2 from '../../images/lindor.png'
+import logo from '../../images/alcantara.png'
+import logo2 from '../../images/rogers.png'
 import '../styles/Miami.css'
 
 const Miami = () => {
   return (
     <div className='mia'>
       <h1>Miami Marlins</h1>
-      <p className='marlins'>The Miami Marlins are a Major League Baseball team. They are based in Queens, NY.
-        They are a member of the Eastern Division of Major League Baseball's National League. The Mets colors
-        are Blue, and Orange. The Mets were founded in 1962.
-        The Mets finished the regular season with a 77-85 record. The Mets were one of the most disappointing teams in the mlb this past season, after trading for lindor and having one of the best pitchers in all of baseball, many people believed that this team was a playoff team and should contend for the division, however that did not happen.  </p>
+      <p className='marlins'>The Miami Marlins are a Major League Baseball team. They are based in Miami, FL.
+        They are a member of the Eastern Division of Major League Baseball's National League. The Marlins colors
+        are Miami Blue, Caliente Red, Slate Gray, and Midnight Black. The Mets were founded in 1991.
+        The Marlins finished the regular season with a 67-95 record. The Marlins are in the rebuilding stages and have been playing alot of young guys to get them experience.
+        These two pitchers will be key for the Marlins going forward.
+      </p>
       <br />
-      <p className='degrom'>
-        Jacob deGrom has been one of the best pitchers in baseball over the last 7 years. Unfortunately for deGrom he suffered right forearm tightness on July 18th which kept him out for the rest of the season.
-        Jacob deGrom was drafted in the 9th round by the Mets in the 2010 MLB draft.
-        Jacob deGrom is a four-time Mlb All-Star; two-time NL CY Young Winner; 2018 NL ERA leader.
-        deGrom has a career 2.50 ERA 1505 strikeouts and a 10.7 K/9.
+      <p className='alcantara'>
+        Sandy Alcantara is one of the most underrated pitchers in all of baseball, one of the reasons why he is underrated is because hes on a bad team that no one wants to watch.
+        Sandy Alcantara was signed by the St.Louis Cardinals on July 2nd, 2013 as a free agent and was later traded to the Miami Marlins on December 14th, 2017.
+        Sandy Alcantara is a one-time Mlb All-Star; 2019 NL leader in Complete Games and Shutouts.
+        Alcantara has a career 3.49 ERA 431 strikeouts and a 8.0 K/9.
       </p>
       <img className='img1' src={logo} alt='img1' />
       <br />
-      <p className='lindor'>
-        Francisco Lindor is a shortstop. Francisco Lindor was traded from the Cleveland Indians to the New York Mets on January 7th 2021.
-        Francisco Lindor was drafted in the 1st round by the Indians in the 2011 MLB draft.
-        Francisco Lindor is a four-time Mlb All-Star; two-time AL Silver Slugger Winner; two-time AL Gold Glove Winner.
-        Lindor has a career .278 BA 158 HR .343 OBP .478 SLG .821 OPS.
+      <p className='rogers'>
+        Trevor Rogers was one of the best rookie pitchers during the 2021 MLB season.
+        Trevor Rogers was drafted in the 1st round by the Marlins in the 2017 MLB draft.
+        Trevor Rogers is a one-time Mlb All-Star; 2021 April NL Rookie of the Month and 2021 May NL Rookie of the Month.
+        Rogers has a career 3.24 ERA 196 strikeouts and a 11.0 K/9.
         <img className='img2' src={logo2} alt='img2' />
       </p>
     </div >
--- sportssubmenu/src/components/mlbteams/Mlb.js
+++ sportssubmenu/src/components/mlbteams/Mlb.js
@@ -1,9 +1,55 @@
 import React from 'react'
+import logo from '../../images/aleast.png'
+import logo2 from '../../images/alc.png'
+import logo3 from '../../images/alwest.png'
+import logo4 from '../../images/nleast.png'
+import logo5 from '../../images/verlander.png'
+import '../styles/Mlb.css'
 
 const Mlb = () => {
   return (
-    <div>
-      <h2>Mlb</h2>
+    <div className='mlb'>
+      <h1>MLB</h1>
+      <p className='league'>The MLB is a professional baseball organization and the oldest major professional sports league in the world. As of 2021, a total of 30 teams play in the MLB- 15 teams in the National League(NL) and 15 in the American League(AL)- with 29 in the United States and 1 in Canada.
+        The MLB was founded in 1903 by the National Association of Baseball Players.
+        The MLB colors are Blue, and Red. The teams are aligned as followed;
+      </p>
+      <br />
+      <p className='ale'>
+        The American League East is one of six divisions in the Major League Baseball. The division was created before the start of the 1969 season along with the American League West division.
+        The New York Yankees hold the most titles from this division with 19 and the Tampa Bay Rays hold the most recent division championship for 2021.
+        The five best players from the AL East are Aaron Judge, Gerrit Cole, Giancarlo Stanton, Rafael Devers and Vladimir Guerrero Jr.
+      </p>
+      <img className='img1' src={logo} alt='img1' />
+      <br />
+      <p className='alc'>
+        The American League Central is one of six divisions in the Major League Baseball. The division was formed in the realignment of 1994, and its teams are all located in the Midwest of the United States.
+        The Cleveland Indians hold the most titles from this division with 10 and the Chicago White Sox hold the most recent division championship for 2021.
+        The five best players from the AL Central are Salvador Perez, Jose Abreu, Jorge Polanco, Lucas Giolito and Carlos Rodon.
+        <img className='img2' src={logo2} alt='img2' />
+        <br />
+      </p>
+      <p className='alw'>
+        The American League West is one of six divisions in the Major League Baseball and is the last division in the American League. The AL West division was founded in 1969.The division has five teams since the 2013 season, but had four teams from 1994 to 2012.
+        The Oakland Athletics hold the most titles from this division with 17 and the Houston Astros hold the most recent division championship for 2021.
+        The five best players from the AL West are Mike Trout, Alex Bregman, Shohei Ohtani, Anthony Rendon and Justin Verlander.
+        <img className='img3' src={logo3} alt='img3' />
+        <br />
+      </p>
+      <p className='nle'>
+        The National League East is one of six divisions in the Major League Baseball. The NL East division was founded in 1969.Along with the American League Central it is one of two divisions to have every member win at least one World Series title.
+        The Atlanta Braves hold the most titles from this division with 16 and the Atlanta Braves hold the most recent division championship for 2021.
+        The five best players from the NL East are Jacob deGrom, Bryce Harper, Ronald Acuna Jr, Austin Riley and Juan Soto.
+        <img className='img4' src={logo4} alt='img4' />
+        <br />
+      </p>
+      <p className='nlc'>
+        The National League East is one of six divisions in the Major League Baseball. The NL East division was founded in 1969.Along with the American League Central it is one of two divisions to have every member win at least one World Series title.
+        The Atlanta Braves hold the most titles from this division with 16 and the Atlanta Braves hold the most recent division championship for 2021.
+        The five best players from the NL East are Jacob deGrom, Bryce Harper, Ronald Acuna Jr, Austin Riley and Juan Soto.
+        <img className='img5' src={logo5} alt='img5' />
+        <br />
+      </p>
     </div>
   )
 }
