2d5d5fe2970e91d9b9d3db5e29496035b6403247
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-10-22 15:05:47 -0400

    Finished the rangers and blue jays

--- sportssubmenu/src/components/mlbteams/Texas.js
+++ sportssubmenu/src/components/mlbteams/Texas.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import logo from '../../images/garcia.png'
+import logo2 from '../../images/falefa.png'
+import '../styles/Texas.css'
 
 const Texas = () => {
   return (
-    <div>
-      <h2>Texas</h2>
-    </div>
+    <div className='tex'>
+      <h1>Texas Rangers</h1>
+      <p className='rangers'>The Texas Rangers are a Major League Baseball team. They are based in Arlington ,TX.
+        They are a member of the Western Division of Major League Baseball's American League. The Rangers colors
+        are Cardinal Blue, and Red. The Rangers were founded in 1961.
+        The Rangers finished the regular season with a 60-102 record. The Texas Rangers are another team that is in rebuilding mode and traded away some of there best players to prepare for the future and get some assets.
+      </p>
+      <br />
+      <p className='garcia'>
+        Adolis Garcia will be one of the key players for the rangers future.
+        Adolis Garcia is a outfielder.
+        Adolis Garcia was signed by the St. Louis Cardinals in February of 2017 as an free agent and then was traded on December 21st, 2019 to the Texas Rangers.
+        Adolis Garcia is a one-time Mlb All-Star; 2021 May AL Rookie of the Month & 2021 April 25th AL Player of the Week.
+        Garcia has a career .237 BA 31 HR .280 OBP .442 SLG .722 OPS.
+      </p>
+      <img className='img1' src={logo} alt='img1' />
+      <br />
+      <p className='falefa'>
+        Isiah Kiner-Falefa is one of the most unique players I have ever seen with more then 500 innings played behind the plate, he has also played shortstop, third base and second base in his short career thus far.
+        Isiah Kiner-Falefa was drafted in the 4th round by the Rangers in the 2013 MLB draft.
+        Isiah Kiner-Falefa 2021 AL leader in singles with 136; 2020 AL Gold Glove winner.
+        Falefa has a career .265 BA 16 HR .316 OBP .354 SLG .670 OPS.
+        <img className='img2' src={logo2} alt='img2' />
+      </p>
+    </div >
   )
 }
 
--- sportssubmenu/src/components/mlbteams/Toronto.js
+++ sportssubmenu/src/components/mlbteams/Toronto.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import logo from '../../images/guerrero.png'
+import logo2 from '../../images/semien.png'
+import '../styles/Toronto.css'
 
 const Toronto = () => {
   return (
-    <div>
-      <h2>Toronto</h2>
-    </div>
+    <div className='tor'>
+      <h1>Toronto Blue Jays</h1>
+      <p className='bluejays'>The Toronto Blue Jays are a Major League Baseball team. They are based in Toronto ,ON.
+        They are a member of the Eastern Division of Major League Baseball's American League. The Blue Jays colors
+        are Blue, Navy Blue and Red. The Blue Jays were founded in 1977.
+        The Blue Jays finished the regular season with a 91-71 record. The Toronto Blue Jays are a really dangerous offensive team, with a handful of explosive players.
+      </p>
+      <br />
+      <p className='guerrero'>
+        Vladimir Guerrero Jr. had alot of hype around him when he was going through the minor league system and might already be in the conversation for top 15 player after a little over 1 year of service time.
+        Vladimir Guerrero Jr. is a first baseman and third baseman.
+        Vladimir Guerrero Jr. was signed by the Toronto Blue Jays in July of 2015 as an free agent.
+        Vladimir Guerrero Jr. is a one-time Mlb All-Star; 2021 AL Home Run leader with 48; 2021 AL On-Base% leader at .401 & 2021 AL Slugging% leader at .601.
+        Guerrero Jr. has a career .289 BA 72 HR .367 OBP .517 SLG .884 OPS.
+      </p>
+      <img className='img1' src={logo} alt='img1' />
+      <br />
+      <p className='semien'>
+        Marcus Semien has his best offensive season of his career hitting a career high in homeruns and rbis as well as SLG %.
+        Marcus Semien was drafted in the 6th round by the White Sox in the 2011 MLB draft, then was traded on December 9th, 2014 to the Oakland Athletics and then was signed as a free agent by the Blue Jays on January 30th, 2021 .
+        Marcus Semien 2021 AL leader in extra base hits with 86; 2021 May AL Player of the Month.
+        Semien has a career .256 BA 160 HR .324 OBP .444 SLG .768 OPS.
+        <img className='img2' src={logo2} alt='img2' />
+      </p>
+    </div >
   )
 }
 
